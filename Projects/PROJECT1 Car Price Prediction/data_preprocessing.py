e5968a9a180597e032a6d14751a2d56c982f3adc
Next Lambda <info@nextlambdaconsulting.com>
2020-09-23 19:10:29 -0400

    Show Project

--- Projects/PROJECT1 Car Price Prediction/data_preprocessing.py
+++ Projects/PROJECT1 Car Price Prediction/data_preprocessing.py
@@ -1,4 +1,7 @@
+import pandas as pd 
 import numpy as np
+from sklearn import preprocessing
+from sklearn.manifold import LocallyLinearEmbedding as LLE
 
 class StandardScaler(object):
     '''Implement standard scaling ((x - mean) / std) scaling.'''
@@ -36,26 +39,59 @@ class NormalizationScaler(object):
         result -= self.minValue
         result /= range
         return result
+    
+    def fit_transform(self, x):
+        self.fit(x)
+        return self.transform(x)
 
 
-class make_model:
+class Make_Model:
     '''Create a column for the make and model and return a new dataframe'''
     
-    def __init__(self, dataframe):
-        self.df = dataframe
+    def __init__(self):
+        self.CarName = None
+    def fit(self):
+        ''' Initialize the lambda function '''
+        self.make = lambda CarName: CarName.split()[0]
+        self.model = lambda CarName: ' '.join(CarName.split()[1:])    
+    def transform(self, x):
+        x['Make'] = x['CarName'].apply(self.make)
+        x['Model'] = x['CarName'].apply(self.model)
+        return x
+    def fit_transform(self, x):
+        self.fit()
+        return self.transform(x)
 
-    def fit(self, row):
-        self.ls = row['CarName'].split()
-        self.make = self.ls[0]
 
-        def model(x):
-            x.pop(0)
-            modelName = ' '.join(x)
-            return modelName
-        self.model = model(self.ls)
-        return self.make, self.model
+class LabelEncode:
+    def __init__(self, df, columns = None):
+        self.df = df
+        self.columns = columns
+        
+    def LabelEncode(self):
+        '''This function takes in a DataFrame and list of columns to encode.
 
-    def transform(self):
-        self.df.apply(self.fit)        
-
+        Input:
+            df: DataFrame
+            columns: list of columns to encode. If None, all the columns will
+                be encoded
+        Output: Encoded DataFrame
+        '''
+        le = preprocessing.LabelEncoder()
+        if self.columns == None:
+            for nm in self.df.columns:
+                self.df[nm] = le.fit_transform(self.df[nm])
+        elif type(self.columns) == list:
+            for nm in self.columns:
+                self.df[nm] = le.fit_transform(self.df[nm])
+        else:
+            self.df[columns] = le.fit_transform(self.df[columns])
         return self.df
+
+
+def One_Hot_Encode(df, columns):
+    pass
+
+def Embedding:
+    pass
+
